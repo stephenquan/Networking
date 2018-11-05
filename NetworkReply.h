6d7c727b45b1480d0101ab8f9f5fdc68552a5599
Stephen Quan <squan@esri.com>
2018-11-05 11:44:51 +1100

    Add additional enums

--- NetworkReply.h
+++ NetworkReply.h
@@ -17,10 +17,87 @@ class NetworkReply: public QObject
 public:
     enum Attribute
     {
-        NetworkReplyAttributeHttpStatusCode = QNetworkRequest::HttpStatusCodeAttribute
+        NetworkReplyAttributeUnknown                        = -1,
+        NetworkReplyAttributeHttpStatusCode                 = QNetworkRequest::Attribute::HttpStatusCodeAttribute,
+        NetworkReplyAttributeHttpReasonPhrase               = QNetworkRequest::Attribute::HttpReasonPhraseAttribute,
+        NetworkReplyAttributeRedirectionTarget              = QNetworkRequest::Attribute::RedirectionTargetAttribute,
+        NetworkReplyAttributeConnectionEncrypted            = QNetworkRequest::Attribute::ConnectionEncryptedAttribute,
+        NetworkReplyAttributeCacheLoadControl               = QNetworkRequest::Attribute::CacheLoadControlAttribute,
+        NetworkReplyAttributeCacheSaveControl               = QNetworkRequest::Attribute::CacheSaveControlAttribute,
+        NetworkReplyAttributeSourceIsFromCache              = QNetworkRequest::Attribute::SourceIsFromCacheAttribute,
+        NetworkReplyAttributeDoNotBufferUploadData          = QNetworkRequest::Attribute::DoNotBufferUploadDataAttribute,
+        NetworkReplyAttributeHttpPipeliningAllowed          = QNetworkRequest::Attribute::HttpPipeliningAllowedAttribute,
+        NetworkReplyAttributeHttpPipeliningWasUsed          = QNetworkRequest::Attribute::HttpPipeliningWasUsedAttribute,
+        NetworkReplyAttributeCustomVerb                     = QNetworkRequest::Attribute::CustomVerbAttribute,
+        NetworkReplyAttributeCookieLoadControl              = QNetworkRequest::Attribute::CookieLoadControlAttribute,
+        NetworkReplyAttributeAuthenticationReuse            = QNetworkRequest::Attribute::AuthenticationReuseAttribute,
+        NetworkReplyAttributeCookieSaveControl              = QNetworkRequest::Attribute::CookieSaveControlAttribute,
+        NetworkReplyAttributeBackgroundRequest              = QNetworkRequest::Attribute::BackgroundRequestAttribute,
+        NetworkReplyAttributeSpdyAllowed                    = QNetworkRequest::Attribute::SpdyAllowedAttribute,
+        NetworkReplyAttributeSpdyWasUsed                    = QNetworkRequest::Attribute::SpdyWasUsedAttribute,
+        NetworkReplyAttributeEmitAllUploadProgressSignals   = QNetworkRequest::Attribute::EmitAllUploadProgressSignalsAttribute,
+        NetworkReplyAttributeFollowRedirects                = QNetworkRequest::Attribute::FollowRedirectsAttribute,
+        NetworkReplyAttributeHTTP2Allowed                   = QNetworkRequest::Attribute::HTTP2AllowedAttribute,
+        NetworkReplyAttributeHTTP2WasUsed                   = QNetworkRequest::Attribute::HTTP2WasUsedAttribute,
+        NetworkReplyAttributeOriginalContentLength          = QNetworkRequest::Attribute::OriginalContentLengthAttribute,
+        NetworkReplyAttributeRedirectPolicy                 = QNetworkRequest::Attribute::RedirectPolicyAttribute,
+#if QT_VERSION >= QT_VERSION_CHECK(5, 11, 0)
+        NetworkReplyAttributeHttp2Direct                    = QNetworkRequest::Attribute::Http2DirectAttribute,
+#endif
     };
 
     Q_ENUM(Attribute)
+
+    enum StatusCode
+    {
+        StatusCodeContinue                      = 100,
+        StatusCodeSwitchingProtocols            = 101,
+
+        StatusCodeOK                            = 200,
+        StatusCodeCreated                       = 201,
+        StatusCodeAccepted                      = 202,
+        StatusCodeNonAuthoritativeInformation   = 203,
+        StatusCodeNoContent                     = 204,
+        StatusCodeResetContent                  = 205,
+        StatusCodePartialContent                = 206,
+
+        StatusCodeMultipleChoices               = 300,
+        StatusCodeMovedPermanently              = 301,
+        StatusCodeFound                         = 302,
+        StatusCodeSeeOther                      = 303,
+        StatusCodeNotModified                   = 304,
+        StatusCodeUseProxy                      = 305,
+        StatusCodeTemporaryRedirect             = 307,
+
+        StatusCodeBadRequest                    = 400,
+        StatusCodeUnauthorized                  = 401,
+        StatusCodePaymentRequired               = 402,
+        StatusCodeForbidden                     = 403,
+        StatusCodeNotFound                      = 404,
+        StatusCodeMethodNotAllowed              = 405,
+        StatusCodeNotAcceptable                 = 406,
+        StatusCodeProxyAuthenticationRequired   = 407,
+        StatusCodeRequestTimeout                = 408,
+        StatusCodeConflict                      = 409,
+        StatusCodeGone                          = 410,
+        StatusCodeLengthRequired                = 411,
+        StatusCodePreconditionFailed            = 412,
+        StatusCodeRequestEntityTooLarge         = 413,
+        StatusCodeRequestURITooLong             = 414,
+        StatusCodeUnsupportedMediaType          = 415,
+        StatusCodeRequestedRangeNotSuitable     = 416,
+        StatusCodeExpectationFailed             = 417,
+
+        StatusCodeInternalServerError           = 500,
+        StatusCodeNotImplemented                = 501,
+        StatusCodeBadGateway                    = 502,
+        StatusCodeServiceUnavailable            = 503,
+        StatusCodeGatewayTimeout                = 504,
+        StatusCodeHTTPVersionNotSupported       = 505
+    };
+
+    Q_ENUM(StatusCode)
+
 
 public:
     NetworkReply(QObject* parent = nullptr);
